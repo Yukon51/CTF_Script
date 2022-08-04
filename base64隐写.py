f656b16e7320d9e7c3bfcaaf0245a1d2a4f783cf
Byxs20 <97766819@qq.com>
2022-08-04 20:31:22 +0800

    modified:   "base64\351\232\220\345\206\231.py"

--- base64隐写.py
+++ base64隐写.py
@@ -11,24 +11,19 @@ args  = parser.parse_args()
 key = string.ascii_uppercase + string.ascii_lowercase + string.digits + "+/"
 
 # 1.获取密文
-# 1.1 读取文件
 with open(args.f, "r") as f:
     data = f.readlines()
 data = [i.replace("\n", "") for i in data]
 
-# 1.2 把cipher填到这个地方
-# cipher = '''
-# '''
-# data = cipher.splitlines()
-
-# base64隐写解码
 '''
 1.依次读取每行，从中提取出隐写位。
 2.如果最后没有‘=’，说明没有隐写位，跳过。
 3.如果最后是一个‘=’，说明有两位隐写位，将倒数第二个字符转化为对应的二进制索引，然后取后两位。
 4.如果最后是两个‘=’，说明有四位隐写位，将倒数第三个字符转化为对应的二进制索引，然后取后四位。
-5.将每行提取出的隐写位依次连接起来，每8位为一组转换为ASCII字符，最后不足8位的丢弃。
+5.记住要补齐8位后取后2位和后4位，如果不补齐的话比如说遇到了B --> "1"，补齐后能得到"01"，不补齐就是"1"，那就错了
+6.将每行提取出的隐写位依次连接起来，每8位为一组转换为ASCII字符，最后不足8位的丢弃。
 '''
+
 binstring = ""
 for cipher in data:
     flag = 0
@@ -38,10 +33,8 @@ for cipher in data:
             flag = 2
     
     if flag == 1:
-        binstring += bin(key.index(cipher[-2])).zfill(8)[-2:]
+        binstring += bin(key.index(cipher[-2]))[2:].zfill(8)[-2:]
     elif flag == 2:
-        binstring += bin(key.index(cipher[-3])).zfill(8)[-4:]
+        binstring += bin(key.index(cipher[-3]))[2:].zfill(8)[-4:]
 
-print(binstring)
-print(len(binstring))
-print(len(binstring) / 8)
+print("".join(chr(int(binstring[i*8:i*8+8], 2)) for i in range(len(binstring) // 8)))
