e4085933d5979c85a58e974e02de32f4fb4f4ebd
Byxs20 <97766819@qq.com>
2022-08-10 18:02:24 +0800

    modified:   "更新大部分实现逻辑"

--- 2进制文本转换ascii.py
+++ 2进制文本转换ascii.py
@@ -1,4 +1,6 @@
 import argparse
+from colorama import init, Fore
+init(autoreset=True)
 
 
 parser = argparse.ArgumentParser()
@@ -8,43 +10,31 @@ args  = parser.parse_args()
 
 cipher = args.t
 
-def BintoAsc(cipher):
-    print(cipher)
-    # 7bit 一字节
-    print("[7Bit]: " + "".join(chr(int(cipher[i:i+7], 2)) for i in range(0, len(cipher), 7)))
-    # 8bit 一字节
-    print("[8Bit]: " + "".join(chr(int(cipher[i:i+8], 2)) for i in range(0, len(cipher), 8)))
-    print()
+def BintoAsc(cipher, bit, reverse=False):
+    ascii_str = ""
+    for i in range(0, len(cipher), bit):
+        bin_str = cipher[i:i + bit][::-1] if reverse else cipher[i:i + bit]
+        # 如果127 >= num >= 32,那就是可见字符,不可见字符统统转换为`~`(波浪线)
+        ascii_str += chr(num) if 127 >= (num := int(bin_str, 2)) >= 32 else chr(126)
+    return ascii_str
 
-def Reverse_BintoAsc(cipher):
-    # 7bit 一字节
-    print("[7Bit]: " + "".join(chr(int(cipher[i:i+7][::-1], 2)) for i in range(0, len(cipher), 7)))
-    # 8bit 一字节
-    print("[8Bit]: " + "".join(chr(int(cipher[i:i+8][::-1], 2)) for i in range(0, len(cipher), 8)))
-    print()
+input_ = input(f"{Fore.GREEN}是否尝试0和1互换后转换Ascii码? (N/y):")
+if input_ not in ["N", "n", ""]:
+    cipher = "".join("1" if i == "0" else "0" for i in cipher)
 
 # 正常情况
-BintoAsc(cipher)
+print(f"{Fore.RED} [7Bit]: {Fore.BLUE}{BintoAsc(cipher, 7)}")
+print(f"{Fore.RED} [8Bit]: {Fore.BLUE}{BintoAsc(cipher, 8)}")
+print()
 
 # 特殊情况1
-input_ = input("是否尝试全部字节倒序后转换Ascii码? (Y/n):")
-if input_ not in ["Y", "y", ""]:
-    exit()
-else:
-    reverse_cipher = cipher[::-1]
-    BintoAsc(reverse_cipher)
+print(f"{Fore.YELLOW} [1].尝试全部字节倒序后转换Ascii码:")
+reverse_cipher = cipher[::-1]
+print(f"{Fore.RED}  [7Bit]: {Fore.BLUE}{BintoAsc(reverse_cipher, 7)}")
+print(f"{Fore.RED}  [8Bit]: {Fore.BLUE}{BintoAsc(reverse_cipher, 8)}")
+print()
+
 # 特殊情况2
-
-input_ = input("是否尝试每个字节依次倒序后转换Ascii码? (Y/n):")
-if input_ not in ["Y", "y", ""]:
-    exit()
-else:
-    Reverse_BintoAsc(cipher)
-
-# 特殊情况3
-input_ = input("是否尝试0和1交换后转换Ascii码? (Y/n):")
-if input_ not in ["Y", "y", ""]:
-    exit()
-else:
-    new_cipher = "".join("1" if i == "0" else "0" for i in cipher)
-    BintoAsc(new_cipher)
+print(f"{Fore.YELLOW} [2].尝试每个字节依次倒序后转换Ascii码:")
+print(f"{Fore.RED}  [7Bit]: {Fore.BLUE}{BintoAsc(cipher, 7, reverse=True)}")
+print(f"{Fore.RED}  [8Bit]: {Fore.BLUE}{BintoAsc(cipher, 8, reverse=True)}")
