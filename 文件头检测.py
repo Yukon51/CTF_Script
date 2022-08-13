430a635a91a3d484883b7311c69e28c0298507a5
Byxs20 <97766819@qq.com>
2022-08-14 00:02:05 +0800

    new file:   "\346\226\207\344\273\266\345\244\264\346\243\200\346\265\213.py" 	deleted:    "\347\214\234\346\265\213\345\274\202\346\210\226.py"

---
+++ 文件头检测.py
@@ -0,0 +1,70 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", type=str, default=None, required=True,
+                    help="输入同级目录下文件的名称")
+args  = parser.parse_args()
+
+file_head = {
+    "FFD8FF": "JPG",
+    "89504E47": "PNG",
+    "47494638": "GIF",
+    "504B0304": "ZIP",
+    "424D": "BMP",
+    "52617221": "RAR",
+    "57415645": "WAV",
+    "41564920": "AVI",
+    "377ABCAF271C": "7Z",
+    "425047": "BPG",
+    "7F454C46": "ELF",
+    "03F30D0A": "PYC",
+    "52494646": "WEBP",
+    "8004": "Python-Pickle",
+    "????????66747970": "MP4/M4A/MOV/3GP",
+}
+
+def save_file(data):
+    with open("out.bin", "wb") as f:
+        print("已帮您自动保存到当前目录!")
+        f.write(data)
+
+with open(args.f, "rb") as f:
+    data = f.read()
+
+# xor异或
+flag = 0
+for head, value in file_head.items():
+    xor_ret = [int(head[j:j+2], 16) ^ data[i] for i, j in enumerate(range(0, len(head), 2)) if head[j:j+2] != "??"]
+
+    if all(item == xor_ret[0] for item in xor_ret):
+        flag = 1
+        print(f"文件可能是: {value} 格式, Xor数值: {hex(xor_ret[0])}")
+
+if not flag:
+    print("没有异或到符合的文件头!")
+
+# not取反
+flag = 0
+not_data = b""
+for i in data:
+    not_data += ((256 - i) % 256).to_bytes(1, byteorder="big", signed=False)
+
+for head, value in file_head.items():
+    ret = []
+    for i, j in enumerate(range(0, len(head), 2)):
+        if head[j:j+2] == "??":
+            ret.append(True)
+        elif int(head[j:j+2], 16) == not_data[i]:
+            ret.append(True)
+        else:
+            ret.append(False)
+
+    if all(ret):
+        flag = 1
+        print(f"文件可能是: {value} 格式, 取反后即可得到!")
+
+if not flag:
+    print("取反没有得到符合的文件头!")
+else:
+    save_file(not_data)
