b4e37e4bfa466eeec13d2c21ae29119852ba9e22
Byxs20 <97766819@qq.com>
2022-08-30 21:40:13 +0800

    modify 变量名

--- 2进制文本转换ascii.py
+++ 2进制文本转换ascii.py
@@ -8,33 +8,33 @@ parser.add_argument('-t', type=str, default=None, required=True,
                     help='输入二进制文本')
 args  = parser.parse_args()
 
-cipher = args.t
+bin_str = args.t
 
-def BintoAsc(cipher, bit, reverse=False):
+def BintoAsc(bin_str, bit, reverse=False):
     ascii_str = ""
-    for i in range(0, len(cipher), bit):
-        bin_str = cipher[i:i + bit][::-1] if reverse else cipher[i:i + bit]
+    for i in range(0, len(bin_str), bit):
+        bin_ = bin_str[i:i + bit][::-1] if reverse else bin_str[i:i + bit]
         # 如果127 >= num >= 32,那就是可见字符,不可见字符统统转换为`~`(波浪线)
-        ascii_str += chr(num) if 127 >= (num := int(bin_str, 2)) >= 32 else chr(126)
+        ascii_str += chr(num) if 127 >= (num := int(bin_, 2)) >= 32 else chr(126)
     return ascii_str
 
 input_ = input(f"{Fore.GREEN}是否尝试0和1互换后转换Ascii码? (N/y):")
 if input_ not in ["N", "n", ""]:
-    cipher = "".join("1" if i == "0" else "0" for i in cipher)
+    bin_str = "".join("1" if i == "0" else "0" for i in bin_str)
 
 # 正常情况
-print(f"{Fore.RED}[7Bit]: {Fore.BLUE}{BintoAsc(cipher, 7)}")
-print(f"{Fore.RED}[8Bit]: {Fore.BLUE}{BintoAsc(cipher, 8)}")
+print(f"{Fore.RED}[7Bit]: {Fore.BLUE}{BintoAsc(bin_str, 7)}")
+print(f"{Fore.RED}[8Bit]: {Fore.BLUE}{BintoAsc(bin_str, 8)}")
 print()
 
 # 特殊情况1
 print(f"{Fore.YELLOW} [1].尝试全部字节倒序后转换Ascii码:")
-reverse_cipher = cipher[::-1]
-print(f"{Fore.RED}  [7Bit]: {Fore.BLUE}{BintoAsc(reverse_cipher, 7)}")
-print(f"{Fore.RED}  [8Bit]: {Fore.BLUE}{BintoAsc(reverse_cipher, 8)}")
+reverse_bin_str = bin_str[::-1]
+print(f"{Fore.RED}  [7Bit]: {Fore.BLUE}{BintoAsc(reverse_bin_str, 7)}")
+print(f"{Fore.RED}  [8Bit]: {Fore.BLUE}{BintoAsc(reverse_bin_str, 8)}")
 print()
 
 # 特殊情况2
 print(f"{Fore.YELLOW} [2].尝试每个字节依次倒序后转换Ascii码:")
-print(f"{Fore.RED}  [7Bit]: {Fore.BLUE}{BintoAsc(cipher, 7, reverse=True)}")
-print(f"{Fore.RED}  [8Bit]: {Fore.BLUE}{BintoAsc(cipher, 8, reverse=True)}")
+print(f"{Fore.RED}  [7Bit]: {Fore.BLUE}{BintoAsc(bin_str, 7, reverse=True)}")
+print(f"{Fore.RED}  [8Bit]: {Fore.BLUE}{BintoAsc(bin_str, 8, reverse=True)}")
