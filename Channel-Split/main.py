a1a95fbc185e64f870f365fe68b23f78819eabe4
Byxs20 <97766819@qq.com>
2022-09-09 10:36:31 +0800

    修改PIL库，改为Opencv

--- Channel-Split/main.py
+++ Channel-Split/main.py
@@ -4,7 +4,7 @@ import argparse
 import itertools
 import numpy as np
 from tqdm import tqdm
-from PIL import Image
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-size', type=int, default=1,
@@ -26,14 +26,22 @@ def makedir(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def split_img(img):
+def get_channel_dic(channel):
+    if channel == 1:
+        channel_dic = ["Gray"]
+    elif channel == 3:
+        channel_dic = ["Blue", "Green", "Red"]
+    elif channel == 4:
+        channel_dic = ["Blue", "Green", "Red", "Alpha"]
+    return channel_dic
+
+def split_channel_bit(img, height, width):
     '''
     分离图片通道，依次8bit分离
     '''
-    np_bit = [[int(i) for i in bin(img.getpixel((x, y)))[2:].zfill(8)] for y, x in itertools.product(range(img.height), range(img.width))]
+    np_bit = [[int(i) for i in bin(img[y, x])[2:].zfill(8)] for y, x in itertools.product(range(height), range(width))]
     np_bit = np.array(np_bit)
-    # 如果为0就是0黑色，如果为1就为255白色
-    np_bit = np.where(np_bit == 0, 0, 255)
+    np_bit = np.where(np_bit == 0, 0, 255) # 如果为0就是0黑色，如果为1就为255白色
     np_bit.astype(np.uint8) # 类型转换
     return np_bit
 
@@ -47,9 +55,14 @@ with tqdm(enumerate(os.listdir(source_path)), desc="Channel Split") as bar:
         save_path = os.path.join(target_path, f"{index}")
         makedir(save_path)
 
-        img = Image.open(rename_path)
-        height, width =  img.size
-        for channel in img.getbands():
-            np_bit = split_img(img.getchannel(channel))
+        # img = Image.open(rename_path)
+        img = cv2.imread(rename_path, -1)
+        height, width, channel = img.shape
+
+        # split channel
+        channel_dic = get_channel_dic(channel)
+        for channel, channel_str in enumerate(channel_dic):
+            channel_img = img[:, :, channel]
+            np_bit = split_channel_bit(channel_img, height, width)
             for i in range(8):
-                cv2.imwrite(os.path.join(save_path, f"{channel}{i}.png"), np_bit[:, 7-i].reshape(width, height, 1))
+                cv2.imwrite(os.path.join(save_path, f"{channel_str} plane {i}.png"), np_bit[:, 7-i].reshape(height, width, 1))
