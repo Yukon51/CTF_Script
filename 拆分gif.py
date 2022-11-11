1fb5856b5503ef07374c6eae35d6034c2359e170
Byxs20 <97766819@qq.com>
2022-11-11 09:13:37 +0800

    更新拆分gif

--- 拆分gif.py
+++ 拆分gif.py
@@ -28,8 +28,6 @@ print(bin_str)'''
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", type=str, default=None, required=True,
                     help="输入同级目录下文件的名称")
-parser.add_argument("-num", type=int, default=None, required=True,
-                    help="输入GIF图片的帧数（使用Stegsolve查看）")
 args  = parser.parse_args()
 
 
@@ -40,15 +38,16 @@ img_save_path = os.path.join(save_path, "./images")
 if not os.path.exists(os.path.join(img_save_path)):
     os.makedirs(os.path.join(img_save_path))
 
+img = Image.open(args.f)
+n_frames = img.n_frames
 # 保存处理脚本
-process_py = process_py.replace("帧数", str(args.num))
+process_py = process_py.replace("帧数", str(n_frames))
 with open(py_save_path, "w") as f:
     f.write(process_py)
 
 # 保存每一帧图片
-img = Image.open(args.f)
 
-with tqdm(range(args.num), desc="Save Image") as num_bar:
+with tqdm(range(n_frames), desc="Save Image") as num_bar:
     for i in num_bar:
         img.seek(i)
         img.save(os.path.join(img_save_path, f"{i}.png"))
