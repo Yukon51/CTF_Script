d2d0acc3583ae77df0c58496a5d37be908ad3265
Byxs20 <97766819@qq.com>
2022-12-03 22:27:46 +0800

    修改为遍历宽高所有情况

--- 2进制绘制二维码.py
+++ 2进制绘制二维码.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import argparse
 import itertools
@@ -7,50 +8,45 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', type=str, default=None, required=True,
                     help='输入文件名称')
-parser.add_argument('-r', type=int, default=None, required=True,
-                    help='输入行数',)
-parser.add_argument('-c', type=int, default=None, required=True,
-                    help='输入列数')
-parser.add_argument('-size', type=int, default=15,
-                    help='图片放大倍数(默认15倍)')
+parser.add_argument('-size', type=int, default=5,
+                    help='图片放大倍数(默认5倍)')
 args = parser.parse_args()
 
-file_name = args.f
-row, col = args.r, args.c
-size = args.size
+file_path = os.path.join(args.f)
 
-# read file
-with open(file_name, "r") as f:
-    data = f.read()
+if not os.path.exists("./out"):
+    os.mkdir("./out")
 
-img1 = np.zeros((row * size, col * size, 3))
-img2 = np.zeros((row * size, col * size, 3))
-
-left_top_point = []
-for i, j in itertools.product(range(0, row * size, size), range(0, col * size, size)):
-    left_top = (j, i)
-    left_top_point.append(left_top)
+# read binary txt
+with open(file_path, "r") as f:
+    data = f.read().strip()
 
 def draw_QR(img, reverse=False):
     for i, v in enumerate(data[:row*col]):
-        right_bottom_point = (
-            left_top_point[i][0] + size, left_top_point[i][1] + size)
+        right_bottom_point = (left_top_point[i][0] + size, left_top_point[i][1] + size)
         if not reverse:
             cv2.rectangle(img, left_top_point[i], right_bottom_point, color=(
                 255, 255, 255), thickness=-1) if v == "0" else cv2.rectangle(img, left_top_point[i], right_bottom_point, color=(0, 0, 0), thickness=-1)
         else:
             cv2.rectangle(img, left_top_point[i], right_bottom_point, color=(0, 0, 0), thickness=-1) if v == "0" else cv2.rectangle(
                 img, left_top_point[i], right_bottom_point, color=(255, 255, 255), thickness=-1)
+    return img
 
-draw_QR(img1, reverse=False)
-draw_QR(img2, reverse=True)
+if __name__ == '__main__':
+    # 计算宽高
+    dic = {X: int(len(data) / X) for X in range(1, len(data)) if len(data) % X == 0}
+    size = args.size
 
-# 添加白色边框
-img1 = cv2.copyMakeBorder(img1, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=(139, 139, 0))
-img2 = cv2.copyMakeBorder(img2, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=(139, 139, 0))
+    for row, col in dic.items():
+        img1, img2 = np.zeros((row * size, col * size, 1)), np.zeros((row * size, col * size, 1))
 
-# 水平拼接
-img = np.concatenate([img1, img2], axis=1)
+        left_top_point = []
+        for i, j in itertools.product(range(0, row * size, size), range(0, col * size, size)):
+            left_top = (j, i)
+            left_top_point.append(left_top)
 
-cv2.imshow("images", img)
-cv2.waitKey(0)
+        draw_QR(img1, reverse=False)
+
+        cv2.imwrite(f"./out/{col}_{row}.png", draw_QR(img1, reverse=False))
+        cv2.imwrite(f"./out/{col}_{row}_reverse.png", draw_QR(img2, reverse=True))
+        print(f"[-] 宽度:{col:6} 高度:{row:6}, 已保存在运行目录out中...")
