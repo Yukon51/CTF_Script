aafb94439ba6a40d90da9dab2ed53b10a3f593c6
Byxs20 <97766819@qq.com>
2023-01-29 22:27:01 +0800

    update Arnold

--- Arnold/main.py
+++ Arnold/main.py
@@ -9,7 +9,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-t', type=str, default=None, required=True, choices=["encode", "decode"],
                     help='encode | decode')
 parser.add_argument('-f', type=str, default=None, required=True,
-                    help='输入文件名称')                  
+                    help='输入文件名称')
+parser.add_argument('-n', type=int, default=1, required=False,
+                    help='输入参数n')
 parser.add_argument('-a', type=int, default=None, required=True,
                     help='输入参数a')
 parser.add_argument('-b', type=int, default=None, required=True,
@@ -17,38 +19,75 @@ parser.add_argument('-b', type=int, default=None, required=True,
 args  = parser.parse_args()
 
 
-def arnold(img, a, b):
-    p = np.zeros((r, c, 3), np.uint8)
+# def arnold(img, n, a, b):
+#     new_img = np.zeros((r, c, 3), np.uint8)
 
-    for i in range(r):
-        for j in range(c):
-            x = (i + b * j) % r
-            y = (a * i + (a * b + 1) * j) % c
-            p[x, y] = img[i, j]
-    return p
+#     for _ in range(n):
+#         for i in range(r):
+#             for j in range(c):
+#                 x = (i + b * j) % r
+#                 y = (a * i + (a * b + 1) * j) % c
+#                 new_img[x, y] = img[i, j]
+#         img = np.copy(new_img)
+#     return new_img
 
-def dearnold(img, a, b):
-    p = np.zeros((r, c, 3), np.uint8)
-    for i in range(r):
-        for j in range(c):
-            x = ((a * b + 1) * i - b * j) % r
-            y = (-a * i + j) % c
-            p[x, y] = img[i, j]
-    return p
+# def dearnold(img, n, a, b):
+#     new_img = np.zeros((r, c, 3), np.uint8)
+
+#     for _ in range(n):
+#         for i in range(r):
+#             for j in range(c):
+#                 x = ((a * b + 1) * i - b * j) % r
+#                 y = (-a * i + j) % c
+#                 new_img[x, y] = img[i, j]
+#         img = np.copy(new_img)
+#     return new_img
+
+def arnold(img, n, a, b):
+    new_img = np.zeros((r, c, 3), np.uint8)
+
+    for _ in range(n):
+        y, x = np.meshgrid(np.arange(c), np.arange(r))
+        new_x = (x + b * y) % r
+        new_y = (a * x + (a * b + 1) * y) % c
+        new_img[new_x, new_y] = img
+        img = np.copy(new_img)
+    return new_img
+
+def dearnold(img, n, a, b):
+    new_img = np.zeros((r, c, 3), np.uint8)
+
+    for _ in range(n):
+        y, x = np.meshgrid(np.arange(c), np.arange(r))
+        new_x = ((a * b + 1) * x - b * y) % r
+        new_y = (-a * x + y) % c
+        new_img[new_x, new_y] = img
+        img = np.copy(new_img)
+    return new_img
+
 
 if __name__ == '__main__':
     img_path = os.path.abspath(args.f)
-    file_name = img_path.split("\\")[-1]
-    img = np.array(Image.open(img_path), np.uint8)
+    file_name = os.path.splitext(img_path)[0].split("\\")[-1]
+    img = np.array(Image.open(img_path).convert("RGB"), np.uint8)[:,:,::-1]
     r, c = img.shape[:2]
-    a, b = args.a, args.b
+    n, a, b = args.n, args.a, args.b
 
     if args.t == "encode":
-        new_img = arnold(img, a, b)
-    elif args.t == "decode":
-        new_img = dearnold(img, a, b)
-    # else:
-    #     print("[-] 图片宽高不一致, 无法进行猫脸变化!")
-    #     exit()
+        new_img = arnold(img, n, a, b)
 
-    cv2.imwrite(f"./Arnold_{a}_{b}_{file_name}", new_img)
+        # b = 0
+        # for a in range(300, 303):
+        #     new_img = arnold(img, n, a, b)
+        #     cv2.imwrite(f"./out_r/{file_name}_{n}_{a}_{b}.png", new_img)
+        # exit()
+    elif args.t == "decode":
+        new_img = dearnold(img, n, a, b)
+
+        # a = 301
+        # for b in range(368, 378):
+        #     new_img = dearnold(img, n, a, b)
+        #     cv2.imwrite(f"./out_r/{file_name}_{n}_{a}_{b}.png", new_img)
+        # exit()
+
+    cv2.imwrite(f"./{file_name}_{n}_{a}_{b}.png", new_img)
--- Arnold/test.py
+++ Arnold/test.py
@@ -1,52 +1,34 @@
-import cv2
-import itertools
+from PIL import Image
 import numpy as np
 
-def g_xy(x, y):
-    yield from np.c_[x, y]
-
-def arnold(img, a, b):
-    r, c = img.shape[:2]
-    new_img = np.zeros((r, c, 3), np.uint8)
-    arr = np.array([[1, a], [b, a*b + 1]])
-    lis = np.array([[y, x] for y, x in itertools.product(range(r), range(c))])
-    out = np.matmul(lis, arr)
-    x = out[:, 0] % r
-    y = out[:, 1] % c
-
-    g = g_xy(x, y)
-
-    for i in range(r):
-        for j in range(c):
-            x, y = next(g)
-            new_img[x, y] = img[i, j]
-    return new_img
-
-def dearnold(img, a, b):
-    r, c = img.shape[:2]
-    new_img = np.zeros((r, c, 3), np.uint8)
-    arr = np.array([[a*b + 1, -a], [-b, 1]])
-    lis = np.array([[y, x] for y, x in itertools.product(range(r), range(c))])
-    out = np.matmul(lis, arr)
-    x = out[:, 0] % r
-    y = out[:, 1] % c
-
-    p = np.c_[x, y]
-
-    g = g_xy(x, y)
-
-    for i in range(r):
-        for j in range(c):
-            x, y = next(g)
-            new_img[x, y] = img[i, j]
-    return new_img
+def arnold(im_file, a, b):
+    img = np.array(Image.open(im_file))
+    height, width, color = img.shape
+    res_img = np.zeros((height, width, color), dtype=int) # 横向变换，右移
+    step = 0
+    for j in range(height):
+        if step == 0:
+            res_img[j] = img[j]
+        else:
+            res_img[j, :step] = img[j, -step:]
+            res_img[j, step:] = img[j, :-step]
+        step = (step+a) % width
+    img = res_img
+    res_img = np.zeros((height, width, color), dtype=int) # 纵向变换，下移
+    step = 0
+    for i in range(width):
+        if step == 0:
+            res_img[:, i] = img[:, i]
+        else:
+            res_img[:step, i] = img[-step:, i]
+            res_img[step:, i] = img[:-step, i]
+        step = (step+b) % height
+    # for i in range(width): #i -> y, j -> x
+    #     for j in range(height):
+    #         new_i = (i + a*j) % width
+    #         new_j = (j + b*new_i) % height
+    #         res_img[new_j, new_i] = img[j, i]
+    Image.fromarray(np.uint8(res_img)).save('res_a{}_b{}.png'.format(a, b))
 
 
-if __name__ == '__main__':
-    a, b = 1, 1
-    img = cv2.imread("./test.png")
-    cv2.imwrite("encode.png", arnold(img, a, b))
-
-    img = cv2.imread("./encode.png")
-    cv2.imwrite("decode.png", dearnold(img, a, b))
-
+arnold("./test.png", 1, 2)
